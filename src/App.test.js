a49e7109a2757d9df85305f21cb9e34a71979401
chamsedine-bouhouch <bouhouchchamseddine@gmail.com>
2024-06-22 17:27:30 +0200

    initial setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders main app', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const mainApp = await screen.findByTestId('main-app');
+  expect(mainApp).toBeInTheDocument();
 });
